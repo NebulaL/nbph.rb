0946b596c80e224e0cafb48f7bb9e51602252d64
NebulaL <65743922+NebulaL@users.noreply.github.com>
2022-07-30 12:39:01 +0000

    add delete_invalid_videos_via_bapi

--- lib/nbph/routine_update_via_bapi_2.rb
+++ lib/nbph/routine_update_via_bapi_2.rb
@@ -1,31 +1,25 @@
+# frozen_string_literal: true
+
 require "./lib/conf/conf"
 require "./lib/bapi/bapi"
 require "./lib/db/connect_db"
 require "./lib/log/log"
 require "oj"
 require "time"
+require "async"
 
-def is_aid_exist(aid)
+def aid_exist?(aid)
   view = Bapi.get_video_morestat(aid)
   (view["code"]).zero? ? True : False
 end
 
-def routine_update_via_bapi(config)
+def add_new_videos_via_bapi(config)
   logger = get_logger("upd")
   db = connect_db(config)
   table_nbph = db[:nbph]
   tid = config["spider"]["tid"]
 
-  logger.info "Now start routine update ,tid=#{tid}"
-
-  if $renv["is_updating"]
-    logger.warn "Lase round has not finished, stop this round"
-    return
-  else
-    $renv["is_updating"] = true
-  end
-
-  logger.info "Now start add new video with tid #{tid}"
+  logger.info "Now start add new videos with tid #{tid}"
   last_aids = table_nbph.reverse(:create).limit(25).map(%i[aid create]).to_h
   db_latest_create = last_aids.values.first
   logger.info "Get last aids: #{last_aids}"
@@ -34,19 +28,12 @@ def routine_update_via_bapi(config)
   page_total = (page["data"]["page"]["count"] / 50.0).ceil
 
   page_num = 1
-  last_aid_list = []
-  last_create_ts = 0
-  last_create_ts_offset = 59
   new_video_count = 0
   cur_create = Time.new
-  loop do
-    break unless page_num <= page_total
-
+  while page_num <= page_total
     page = Oj.load(Bapi.get_archive_rank_by_partion(tid, page_num, 50))
 
-    loop do
-      break if page["data"]["archives"].is_a? Array
-
+    until page["data"]["archives"].is_a? Array
       logger.warn "pn=#{page_num}, page['data']['archives'] isn't a Array, re-call after 1s"
       sleep 1
       page = Oj.load(Bapi.get_archive_rank_by_partion(tid, page_num, 50))
@@ -70,10 +57,13 @@ def routine_update_via_bapi(config)
 
     table_nbph.multi_insert video_list
     logger.info "Add new videos #{video_list}"
+    new_video_count += video_list.size
 
     if cur_create < db_latest_create
-      db_cur_count = table_nbph.where { create < cur_create }.count
+      db_count = table_nbph.count
       bapi_count = page["data"]["page"]["count"].to_i
+      # db_cur_count = table_nbph.where { create < cur_create }.count
+      # bapi_cur_count = bapi_count - (page['data']['page']['num'].to_i * 50)
       if db_count < bapi_count
         logger.info("db_count < bapi_count (#{db_count} #{bapi_count}), next/2")
         next
@@ -82,11 +72,78 @@ def routine_update_via_bapi(config)
       break
     end
 
-    page_total = (page["data"]["page"]["count"] / 50) + 1
+    # page_total = (page['data']['page']['count'] / 50) + 1
     page_num += 1
   end
 
-  logger.info "Finish routine update #{tid} tid."
-ensure
-  $renv["is_updating"] = false
+  logger.info "Finish add new videos via bapi #{tid} tid."
+end
+
+def delete_invalid_videos_via_bapi(config)
+  logger = get_logger("upd")
+  db = connect_db(config)
+  table_nbph = db[:nbph]
+  tid = config["spider"]["tid"]
+
+  logger.info "Now start delete invalid video with tid #{tid}"
+
+  db_count = table_nbph.count
+
+  page = Oj.load(Bapi.get_archive_rank_by_partion(tid, 1, 50))
+  bapi_count = page["data"]["page"]["count"].to_i
+  page_total = (bapi_count / 50.0).ceil
+
+  logger.info "Get db_count=#{db_count}, bapi_count=#{bapi_count}"
+
+  invalid_count = db_count - bapi_count
+  if invalid_count.positive?
+    page_num = 1
+    loop do
+      break if (page_num >= page_total) || !invalid_count.positive?
+
+      pages = []
+      Async do
+        5.times do |i|
+          Async do
+            page =
+              Oj.load(Bapi.get_archive_rank_by_partion(tid, page_num + i, 50))
+
+            loop do
+              break if page["data"]["archives"].is_a? Array
+
+              logger.warn "pn=#{page_num}, page['data']['archives'] isn't a Array, re-call after 1s"
+              sleep 1
+              page =
+                Oj.load(Bapi.get_archive_rank_by_partion(tid, page_num + i, 50))
+            end
+            pages[i] = page
+          end
+        end
+      end
+      pages_aids = []
+      pages.each do |i|
+        i["data"]["archives"].each { |i| page_aids.push(i.fetch(:aid)) }
+      end
+
+      create_ts_from =
+        Time.parse(pages[0]["data"]["archives"][0]["create"]) + 59
+      create_ts_to = Time.parse(pages[-1]["data"]["archives"][-1]["create"])
+
+      db_aids =
+        table_nbph
+          .where { Sequel.&(create <= create_ts_from, create >= create_ts_to) }
+          .map(:aid)
+
+      invalid_aids = db_aids - pages_aids
+
+      table_nbph.where(aid: invalid_aids).delete
+
+      invalid_count -= invalid_aids.size
+
+      page_num += pages.size
+    end
+  else
+    logger.info "No invalid video to delete"
+  end
+  logger.info "Finish delete invalid video with tid #{tid}!"
 end
