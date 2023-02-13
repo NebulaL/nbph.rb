ab2b79496feae5a9e5a9d1a026e5c44259cec34b
NebulaL <65743922+NebulaL@users.noreply.github.com>
2023-02-13 00:56:33 +0000

    add async preload to init_via_bapi

--- Gemfile
+++ Gemfile
@@ -2,12 +2,15 @@
 
 source "https://rubygems.org"
 
-gem "faraday"
-gem "oj"
-gem "logging"
-gem "pg"
-gem "sequel"
+gem "faraday", "~> 2.3.0"
+gem "oj", "~> 3.13.17"
+gem "logging", "~> 2.3.1"
+gem "pg", "~> 1.4.1"
+gem "sequel", "~> 5.58.0"
 
 gem "ruby-lsp", "~> 0.2.0", :group => :development
 
-gem "sqlite3"
+gem "sqlite3", "~> 1.6.0"
+
+gem "async", "~> 2.3"
+gem "async-http", "~> 0.60.1"
--- lib/nbph/init_via_bapi_3.rb
+++ lib/nbph/init_via_bapi_3.rb
@@ -6,6 +6,9 @@ require './lib/db/connect_db'
 require './lib/db/create_table'
 require 'oj'
 require 'time'
+require 'async'
+require 'async/barrier'
+require 'async/http/internet'
 
 def init_via_bapi(config)
     db = connect_db(config)
@@ -19,34 +22,55 @@ def init_via_bapi(config)
     last_aid_list = []
     last_create = 0
     c_seq = 0
+    x = 50
 
     until page_num >= page_total
-        cur_page = Oj.load(Bapi.get_archive_rank_by_partion(tid, page_num, 50))['data']
-        cur_page_vids = cur_page['archives']
-        vid_list = []
-        last_aid_list.clear
-
-        cur_page_vids.each do |video|
-            aid = video['aid'].to_i
-            next if last_aid_list.include?(aid)
-            last_aid_list.push aid
-            create = Time.parse video['create']
-
-            if create == last_create
-                c_seq += 1
-            else
-                last_create = create
-                c_seq = 0
+        t=Time.now
+        pages = (page_num..(page_num+x)).to_a
+        cur_lastest = (page_num+x)
+        dats = {}
+        Async do
+            internet = Async::HTTP::Internet.new
+            barrier = Async::Barrier.new
+                pages.each do |pn|
+                barrier.async do
+                    response = internet.get "http://api.bilibili.com/archive_rank/getarchiverankbypartion?jsonp=jsonp&tid=30&pn=#{pn}&ps=50"
+                    dats[pn] = response.read
+                end
             end
-            vid_list.push(
-                { aid: aid, tid: tid, create: create, c_seq: c_seq }
-            )
+            barrier.wait
+        ensure
+            internet&.close
         end
+        until page_num >= cur_lastest
+            cur_page = Oj.load(dats[page_num])['data']
+            cur_page_vids = cur_page['archives']
+            vid_list = []
+            last_aid_list.clear
 
-        table_nbph.multi_insert vid_list
+            cur_page_vids.each do |video|
+                aid = video['aid'].to_i
+                next if last_aid_list.include?(aid)
+                last_aid_list.push aid
+                create = Time.parse video['create']
 
-        page_total = (cur_page['page']['count'] / 50) + 1
-        puts "Page #{page_num} / #{page_total} done."
-        page_num += 1
+                if create == last_create
+                    c_seq += 1
+                else
+                    last_create = create
+                    c_seq = 0
+                end
+                vid_list.push(
+                    { aid: aid, tid: tid, create: create, c_seq: c_seq }
+                )
+            end
+
+            table_nbph.multi_insert vid_list
+
+            page_total = (cur_page['page']['count'] / 50) + 1
+            puts "Page #{page_num} / #{page_total} done."
+            page_num += 1
+        end
+        puts Time.now-t
     end
 end
